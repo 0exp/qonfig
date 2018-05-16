84ecf70d689455c19e76d78b4cac314559a23960
Rustam Ibragimov <iamdaiver@icloud.com>
2018-05-16 23:26:08 +0300

    Inheritance spec

---
+++ spec/features/inheritance_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+describe 'Inheritance' do
+  specify 'config inheritance works correclty' do
+    class FrameworkConfig < Qonfig::DataSet
+      setting :version, '0.1.0'
+
+      setting :defaults do
+        setting :path, '/a/b/c'
+      end
+    end
+
+    class UnknownConfig < Qonfig::DataSet
+      setting :unkown_data, true
+    end
+
+    class SharedConfig < Qonfig::DataSet
+      setting :google_api do
+        setting :token, 'test-google-api'
+      end
+
+      setting :admin_access_required, false
+
+      compose FrameworkConfig
+    end
+
+    class ClientConfig < SharedConfig
+      compose UnknownConfig
+
+      setting :google_api do
+        setting :client_token, 'client-test-google-api'
+      end
+
+      setting :defaults, nil
+    end
+
+    client_config = ClientConfig.new
+
+    client_config.settings.tap do |config|
+      # own settings
+      expect(config.google_api.client_token).to eq('client-test-google-api')
+      expect(config.defaults).to eq(nil)
+      expect(config[:google_api][:client_token]).to eq('client-test-google-api')
+      expect(config[:defaults]).to eq(nil)
+
+      # inherited settings
+      expect(config.google_api.token).to eq('test-google-api')
+      expect(config.admin_access_required).to eq(false)
+      expect(config[:google_api][:token]).to eq('test-google-api')
+      expect(config[:admin_access_required]).to eq(false)
+
+      # inherited composition
+      expect(config.version).to eq('0.1.0')
+      expect(config[:version]).to eq('0.1.0')
+
+      # own composition
+      expect(config.unkown_data).to eq(true)
+      expect(config[:unkown_data]).to eq(true)
+
+      # re-configuration
+      config.google_api.client_token = 'none'
+      config.defaults = { a: 1 }
+      config.google_api.token = 'anti-hype'
+      config.admin_access_required = true
+      config.version = '0.2.0'
+      config.unkown_data = nil
+
+      expect(config.google_api.client_token).to eq('none')
+      expect(config.defaults).to match(a: 1)
+      expect(config.google_api.token).to eq('anti-hype')
+      expect(config.admin_access_required).to eq(true)
+      expect(config.version).to eq('0.2.0')
+      expect(config.unkown_data).to eq(nil)
+
+      expect(config[:google_api][:client_token]).to eq('none')
+      expect(config[:defaults]).to match(a: 1)
+      expect(config[:google_api][:token]).to eq('anti-hype')
+      expect(config[:admin_access_required]).to eq(true)
+      expect(config[:version]).to eq('0.2.0')
+      expect(config[:unkown_data]).to eq(nil)
+    end
+  end
+end
