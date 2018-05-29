72038b24fba628daa24643e455b636fc1bda913d
Rustam Ibragimov <iamdaiver@icloud.com>
2018-05-29 16:15:11 +0300

    Qonfig::DataSet#dig specs

--- spec/features/config_definition_spec.rb
+++ spec/features/config_definition_spec.rb
@@ -271,4 +271,44 @@ describe 'Config definition' do
       config.configure { |conf| conf.database.hostname = double }
     end.not_to raise_error
   end
+
+  specify '#dig functionality' do
+    class DiggingConfig < Qonfig::DataSet
+      setting :db do
+        setting :creds do
+          setting :user, 'D@iVeR'
+          setting :password, 'test123'
+          setting :data, { test: false }
+        end
+      end
+    end
+
+    config = DiggingConfig.new
+
+    # acces to a value
+    expect(config.dig(:db, :creds, :user)).to eq('D@iVeR')
+    expect(config.dig('db', :creds, 'password')).to eq('test123')
+    expect(config.dig('db', 'creds', 'data')).to match(test: false)
+
+    # access to the settings
+    expect(config.dig(:db, :creds)).to be_a(Qonfig::Settings)
+    expect(config.dig(:db)).to be_a(Qonfig::Settings)
+
+    # try to dig into the hash value (setting with a hash value)
+    expect { config.dig(:db, :creds, :user, :test) }.to raise_error(Qonfig::UnknownSettingError)
+
+    # dig with empty key lists
+    expect { config.dig(*[]) }.to raise_error(Qonfig::ArgumentError)
+    expect { config.dig }.to raise_error(Qonfig::ArgumentError)
+
+    # dig into unexistent option
+    expect do
+      config.dig(:db, :creds, :session)
+    end.to raise_error(Qonfig::UnknownSettingError)
+
+    # dig into unexistent option
+    expect do
+      config.dig(:a, :b, :c, :d)
+    end.to raise_error(Qonfig::UnknownSettingError)
+  end
 end
