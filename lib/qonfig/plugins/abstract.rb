48afc274c8882cce45307333810b770d6eb54042
Rustam Ibragimov <iamdaiver@icloud.com>
2018-06-15 13:31:30 +0300

    [Plugins] useless tempalte_method methods

--- lib/qonfig/plugins/abstract.rb
+++ lib/qonfig/plugins/abstract.rb
@@ -11,24 +11,6 @@ module Qonfig
         # @api private
         # @since 0.4.0
         def load!
-          load_dependencies!
-          load_code!
-        end
-
-        private
-
-        # @retirn [void]
-        #
-        # @api private
-        # @since 0.4.0
-        def load_dependencies!
-        end
-
-        # @return [void]
-        #
-        # @api private
-        # @since 0.4.0
-        def load_code!
         end
       end
     end
