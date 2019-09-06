8c6556aa0b4d0da809ddcfc6b15effac60183f2b
Rustam Ibragimov <iamdaiver@icloud.com>
2019-09-06 15:10:32 +0300

    [subset] feature and specs

---
+++ spec/features/subset_functionality_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/LineLength
+describe '(#subset)-functionality' do
+  specify '#slice/#slice_value functionality works as expected :)' do
+    class SubsetableConfig < Qonfig::DataSet
+      setting :db do
+        setting :creds do
+          setting :user, 'D@iVeR'
+          setting :data, test: false
+        end
+      end
+
+      setting :adapter, :sidekiq
+    end
+
+    config = SubsetableConfig.new
+
+    # access to the subset (with indifferent keys type)
+    expect(config.subset(:db, ['db', :creds], ['db', 'creds', :user], 'adapter')).to match(
+      'db' => { 'creds' => { 'user' => 'D@iVeR', 'data' => { test: false } } }, # :db
+      'creds' => { 'user' => 'D@iVeR', 'data' => { test: false } }, # ['db', :creds]
+      'user' => 'D@iVeR', # ['db', 'creds', :user]
+      'adapter' => :sidekiq # 'adepter'
+    )
+
+    # try to use subset with with unexistent keys
+    # NOTE: :megazavr key does not exist
+    expect { config.subset([:db, :creds, :megazavr]) }.to raise_error(Qonfig::UnknownSettingError)
+    # NOTE: :test key does not exist
+    expect { config.subset(:db, :test) }.to raise_error(Qonfig::UnknownSettingError)
+
+    # you cant use subset operation over setting values - you can do it only over the setting keys!
+    expect { config.subset([:db, :creds, :data, :test]) }.to raise_error(Qonfig::UnknownSettingError)
+
+    # subset invokation with empty key list
+    # rubocop:disable Lint/UnneededSplatExpansion
+    expect(config.subset(*[])).to eq({})
+    expect(config.subset).to eq({})
+    # rubocop:enable Lint/UnneededSplatExpansion
+
+    # subset invokation over unexistent option
+    expect { config.subset([:db, :creds, :session]) }.to raise_error(Qonfig::UnknownSettingError)
+    expect { config.subset([:a, :b, :c, :d]) }.to raise_error(Qonfig::UnknownSettingError)
+  end
+end
+# rubocop:enable Metrics/LineLength
