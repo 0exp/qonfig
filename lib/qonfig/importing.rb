11f93d4a1c2792602a1d842aab1b85d8ddc0eb21
Rustam Ibragimov <iamdaiver@gmail.com>
2019-09-20 16:26:43 +0300

    [importing] in progress :)

--- lib/qonfig/importing.rb
+++ lib/qonfig/importing.rb
@@ -3,4 +3,19 @@
 # @api public
 # @since 0.17.0
 module Qonfig::Importing
+  class << self
+    # @param base_klass [Class]
+    # @return [void]
+    #
+    # @api private
+    # @since 0.17.0
+    def included(base_klass)
+      base_klass.extend(DSL)
+    end
+  end
+
+  # @api private
+  # @since 0.17.0
+  module DSL
+  end
 end
