094bbafe9b59ffc23daf85c354dcca8a4747c5f4
Rustam Ibragimov <iamdaiver@gmail.com>
2019-09-21 21:44:19 +0300

    [exporting] import => export

--- lib/qonfig/exporting.rb
+++ lib/qonfig/exporting.rb
@@ -2,7 +2,7 @@
 
 # @api public
 # @since 0.17.0
-module Qonfig::Importing
+module Qonfig::Exporting
   class << self
     # @param base_klass [Class]
     # @return [void]
@@ -17,5 +17,14 @@ module Qonfig::Importing
   # @api private
   # @since 0.17.0
   module DSL
+    # @param config [Qonfig::DataSet]
+    # @param setting_keys [Array<String,Symbol>]
+    # @return [void]
+    #
+    # @api public
+    # @since 0.17.0
+    def export_config(config, *setting_keys)
+
+    end
   end
 end
