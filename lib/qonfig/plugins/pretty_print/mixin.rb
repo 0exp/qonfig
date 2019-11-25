e1ea2bd67f32aeeb6f0589673d993b6dc69ef2b9
Rustam Ibragimov <iamdaiver@gmail.com>
2019-11-25 16:14:53 +0300

    [pretty-print] specs

--- lib/qonfig/plugins/pretty_print/mixin.rb
+++ lib/qonfig/plugins/pretty_print/mixin.rb
@@ -3,24 +3,20 @@
 # @api private
 # @since 0.19.0
 module Qonfig::Plugins::PrettyPrint::Mixin
-  # @param pp [Pry::ColorPrinter] In some cases this is a Pry::ControlPrinter ¯\_(ツ)_/¯
+  # @param pp [?] Suitable for Ruby's PP module
   # @return [void]
   #
   # @api public
   # @since 0.19.0
   def pretty_print(pp)
-    if method(:inspect).owner != Qonfig::DataSet.instance_method(:inspect).owner
-      super
-    else
-      pp.object_address_group(self) do
-        pp.seplist(keys, proc { pp.text(',') }) do |key|
-          pp.breakable(' ')
-          pp.group(1) do
-            pp.text(key)
-            pp.text(':')
-            pp.breakable
-            pp.pp(self[key])
-          end
+    pp.object_address_group(self) do
+      pp.seplist(keys, proc { pp.text(',') }) do |key|
+        pp.breakable(' ')
+        pp.group(1) do
+          pp.text(key)
+          pp.text(':')
+          pp.breakable
+          pp.pp(self[key])
         end
       end
     end
---
+++ spec/features/plugins/pretty_print_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+describe 'Plugins(:pretty_print): Pretty print :)' do
+  before { Qonfig.plugin(:pretty_print) }
+
+  before do
+    stub_const('PrettyPrintableConfig', Class.new(Qonfig::DataSet) do
+      setting :api do
+        setting :domain, 'google'
+        setting :creds do
+          setting :token, 'a0sdj10k@'
+          setting :login, 'D2'
+        end
+      end
+
+      setting :database do
+        setting :adapter, 'pg'
+      end
+
+      setting :logging, false
+      setting :author, nil
+    end)
+  end
+
+  let(:pretty_printer_output) { StringIO.new }
+
+  specify 'shows config settings in beatufied format' do
+    config = PrettyPrintableConfig.new
+    value_space = SpecSupport.from_object_id_space_to_value_space(config)
+    PP.pp(config, pretty_printer_output)
+
+    expect(pretty_printer_output.string).to eq(
+      "#<PrettyPrintableConfig:0x#{value_space}\n" \
+      " api.domain: \"google\",\n" \
+      " api.creds.token: \"a0sdj10k@\",\n" \
+      " api.creds.login: \"D2\",\n" \
+      " database.adapter: \"pg\",\n" \
+      " logging: false,\n" \
+      " author: nil>\n"
+    )
+  end
+end
