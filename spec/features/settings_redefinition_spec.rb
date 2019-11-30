f30ea4a8c05cf54b33afcdacfcc278be18560fe5
Rustam Ibragimov <iamdaiver@gmail.com>
2019-11-30 22:07:57 +0300

    [settings-redefinition] specs

--- spec/features/settings_redefinition_spec.rb
+++ spec/features/settings_redefinition_spec.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
 describe 'Settings redefinition' do
-  pending 'write specs :)'
+  specify do
+    class BaseRedefinableConfig < Qonfig::DataSet
+      setting :nested do
+        setting :some_key, 100_500
+      end
+    end
+
+    class ChildRedefinitionConfig < BaseRedefinableConfig
+      re_setting :nested, :some_value
+    end
+
+    class AnotherChildRedefinitionConfig < BaseRedefinableConfig
+      re_setting :nested do
+        setting :another_key, 'test'
+      end
+    end
+
+    child_redefinition_config = ChildRedefinitionConfig.new
+    expect(child_redefinition_config[:nested]).to eq(:some_value)
+
+    another_child_redefinition_config = AnotherChildRedefinitionConfig.new
+    expect(another_child_redefinition_config[:nested][:another_key]).to eq('test')
+  end
 end
