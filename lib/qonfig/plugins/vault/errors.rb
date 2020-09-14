1baa054f546007af0c1242b285f8dc240f5395c7
dude <VanyaZ158@gmail.com>
2020-09-14 12:21:51 +0300

    Fix last discus

--- lib/qonfig/plugins/vault/errors.rb
+++ lib/qonfig/plugins/vault/errors.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # @api public
-# @since 0.17.0
+# @since 0.25.0
 module Qonfig
   # @api public
-  # @since 0.21.0
+  # @since 0.25.0
   VaultLoaderError = Class.new(Vault::VaultError)
 end
